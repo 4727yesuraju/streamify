10d07ee0cff3e3f51cfe7cff1b409ee757d8043c
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2025-08-14 21:53:07 +0530

    onboard

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -79,3 +79,47 @@ export async function logout(req, res) {
   res.clearCookie("jwt");
   res.status(200).json({ success: true, message: "Logout Successgul" });
 }
+
+export async function onboard(req, res) {
+  try {
+    const userId = req.user._id;
+    const { fullName, bio, nativeLanguage, learningLanguage, location } =
+      req.body;
+    if (
+      !fullName ||
+      !bio ||
+      !nativeLanguage ||
+      !learningLanguage ||
+      !location
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "leanringLanguage",
+          !location && "Location",
+        ].filter(Boolean),
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        ...req.body,
+        isOnboarded: true,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser)
+      return res.status(404).json({ message: "User not found" });
+
+    //update userinfo in streamer
+    res.status(200).json({ success: true, user: updatedUser });
+  } catch (error) {
+    console.error("Onboarding error : ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 // import { config } from "dotenv";
 // config();
 import "dotenv/config";
-
+import cookieParser from "cookie-parser";
 //router
 import authRoutes from "./router/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -10,6 +10,7 @@ import { connectDB } from "./lib/db.js";
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log("server is running at port : ", PORT);
